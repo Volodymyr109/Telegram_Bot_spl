8c1f299ddf8bb9a5675b39a3af30a0d818358f9d
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-04-03 14:05:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,76 @@
-import Constants as keys
-from telegram.ext import*
-import Responses as R
-
-print("Bot Started...")
+import telebot
+from mg import get_map_cell
 
 
-def start_command(update, context):
-    update.message.reply_text('Type someth to started!')
+bot = telebot.TeleBot('<5863679411:AAEGMZYt6MUzvxurokYbaev4Tv4sk47s5Lg>')
+cols, rows = 8, 8
 
+keyboard = telebot.types.InlineKeyboardMarkup()
+keyboard.row( telebot.types.InlineKeyboardButton('←', callback_data='left'),
+			  telebot.types.InlineKeyboardButton('↑', callback_data='up'),
+			  telebot.types.InlineKeyboardButton('↓', callback_data='down'),
+			  telebot.types.InlineKeyboardButton('→', callback_data='right') )
 
-def help_command(update, context):
-    update.message.reply_text('If you need help - ask for it in Google')
+maps = {}
 
+def get_map_str(map_cell, player):
+	map_str = ""
+	for y in range(rows * 2 - 1):
+		for x in range(cols * 2 - 1):
+			if map_cell[x + y * (cols * 2 - 1)]:
+				map_str += "⬛"
+			elif (x, y) == player:
+				map_str += "🔴"
+			else:
+				map_str += "⬜"
+		map_str += "\n"
 
-def handle_message(update, context):
-    text = str(update.message.text).lower()
-    response = R.sample_responses(text)
+	return map_str
 
-    update.message.reply_text(response)
+@bot.message_handler(commands=['play'])
+def play_message(message):
+	map_cell = get_map_cell(cols, rows)
 
+	user_data = {
+		'map': map_cell,
+		'x': 0,
+		'y': 0
+	}
 
-def error(update, context):
-    print(f"Update {update} caused error {context.error}")
+	maps[message.chat.id] = user_data
 
+	bot.send_message(message.from_user.id, get_map_str(map_cell, (0, 0)), reply_markup=keyboard)
 
-def main():
-    updater = Updater(keys.API_KEY, use_context=True)
-    dp = updater.dispatcher
+@bot.callback_query_handler(func=lambda call: True)
+def callback_func(query):
+	user_data = maps[query.message.chat.id]
+	new_x, new_y = user_data['x'], user_data['y']
 
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
+	if query.data == 'left':
+		new_x -= 1
+	if query.data == 'right':
+		new_x += 1
+	if query.data == 'up':
+		new_y -= 1
+	if query.data == 'down':
+		new_y += 1
 
-    dp.add_handler(MessageHandler(Filters.text, handle_message))
+	if new_x < 0 or new_x > 2 * cols - 2 or new_y < 0 or new_y > rows * 2 - 2:
+		return None
+	if user_data['map'][new_x + new_y * (cols * 2 - 1)]:
+		return None
 
-    dp.add_error_handler(error)
+	user_data['x'], user_data['y'] = new_x, new_y
 
-    updater.start_polling()
-    updater.idle()
+	if new_x == cols * 2 - 2 and new_y == rows * 2 - 2:
+		bot.edit_message_text( chat_id=query.message.chat.id,
+							   message_id=query.message.id,
+							   text="Вы выиграли" )
+		return None
 
+	bot.edit_message_text( chat_id=query.message.chat.id,
+						   message_id=query.message.id,
+						   text=get_map_str(user_data['map'], (new_x, new_y)),
+						   reply_markup=keyboard )
 
-main()
+bot.polling(none_stop=False, interval=0)
